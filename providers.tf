06f878bd8867b7a2c638e9f9f54e270f6c10f681
vincentvgit <vincentsimplon@gmail.com>
2024-04-30 15:17:58 +0200

    add providers

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+   required_version = ">= 0.12"
+   required_providers {
+      azurerm = ">2.5"
+   }
+}
+
+provider "azurerm" {
+   
+   features {}
+}
+variable "location" {
+   type = string
+   description = "Region"
+   default = "westeurope"
+}
